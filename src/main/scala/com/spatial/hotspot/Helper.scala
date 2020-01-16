da44faf5134a4d0e1d155d655478942851473346
phassan <hassan.cse05@gmail.com>
2020-01-16 16:39:39 -0700

    Update Helper.scala

--- src/main/scala/com/spatial/hotspot/Helper.scala
+++ src/main/scala/com/spatial/hotspot/Helper.scala
@@ -30,34 +30,43 @@ object Helper extends java.io.Serializable {
     try {
 
       var start_time = Calendar.getInstance().getTime()
+      // Step 1: Reading CSV file and saving the data in RDD[String].
       val inputCsvFile = sc.textFile(inputFile)
+      // Step 2: Remove header from the data.
       val header = inputCsvFile.first()
       val inputRows = inputCsvFile.filter { x => x != header }
-
+      //Step 3:
       var inputRowsLst = inputRows.map(line => {
         //        var lineArr = line.split(";")
         var lineArr = line.split(",")
+        // For all rows in data, rowConvert() converts pickup date time, longitude and latitude to cell coordinate 
+        // with corresponding number of pickup occurrences of that coordinate.
         rowConvert(lineArr(1), lineArr(5), lineArr(6))
       })
 
+      // Step 4: Filter out the data that does not have any pickup occurrence. 
       val inputRowsLstFinal = inputRowsLst.filter { case (x, y) => x != "" }
 
+      // Step 5: Group pickup occurrences with their respective cell coordinates.
       val inputRowsGpBy = inputRowsLstFinal.groupByKey()
 
+      //Step 6: Calculate the total number occurrences for each cell coordinate.
       val inputRowsMap = inputRowsGpBy.map { case (x, y) => (x, y.size) }
 
       var total_cell_no = ((lat_end - lat_start) / cell_length) * ((lon_end - lon_start) / cell_length) * no_of_days
 
-      //calculating standard deviation
+      // Step 7: Calculate Mean and Standard Deviation.
       var x_bar = inputRowsMap.map((_._2)).sum() / total_cell_no
       var tmp_x_bar = inputRowsMap.map(x => Math.pow(x._2, 2)).sum()
       var S = Math.sqrt((tmp_x_bar / total_cell_no) - Math.pow(x_bar, 2));
 
+      // Step 8: Collect data as Map.
       var inputRowsMapVal = inputRowsMap.collectAsMap()
       //      inputRowsMapVal.foreach(x => {
       //        println(x._1 + " --> " + x._2)
       //      })
 
+      // Step 9: Create cell coordinates for the whole cube.
       var cube = for (day <- 1 to no_of_days by 1; lonVal <- lon_start to lon_end - cell_length by cell_length; latVal <- lat_start to lat_end - cell_length by cell_length) yield {
 
         var cellKey_y = ((latVal - lat_start) / cell_length).toInt
@@ -70,7 +79,8 @@ object Helper extends java.io.Serializable {
       //      cube.foreach(x => {
       //        println(x._1 + " --> " + x._2)
       //      })
-
+      
+      // Step 10: Assign the total pickup occurrence in each grid cell.
       var finalInputRowsRDDVal = sc.parallelize(cube).map { m =>
         (m._1 ->
           {
@@ -83,16 +93,21 @@ object Helper extends java.io.Serializable {
       //      finalInputRowsRDDVal.filter(x => x._2 != 0).foreach(x => {
       //        println(x._1 + " --> " + x._2)
       //      })
-
+      
+      // Step 11: Collect cell coordinates with their total pickup occurrences as map.
       var finalInputRowsMapVal = finalInputRowsRDDVal.collectAsMap()
 
       //      finalInputRowsMapVal.foreach(x => {
       //        println(x._1 + " --> " + x._2)
       //      })
-
+      
+      // Step 12: Calculate zscore for each cell coordinate of the cube.
       var inputRowsMap_Zscore = finalInputRowsRDDVal.map(x => (calculateOutputKey(x._1), calculateZscore(x._1, finalInputRowsMapVal, total_cell_no, x_bar, S)))
 
+      // Step 13: Sort the result in descending order based on zscore.
       val result = inputRowsMap_Zscore.sortBy(r => (-r._2))
+      
+      // Step 14: Return top 50 records and saving the final result in a single output file.
       val finalResult = result.take(50)
 
       sc.parallelize(finalResult).map(w => w._1 + "," + w._2).coalesce(1).saveAsTextFile(outputPath + "result.txt");
